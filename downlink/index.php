77cc80ba422338e4be695bd3ef5e5c6d41560ba6
Matt Rude <matt@mattrude.com>
2022-05-19 01:12:29 +0000

    minor cleanup of download

--- downlink/index.php
+++ downlink/index.php
@@ -6,19 +6,17 @@ header("Pragma: no-cache");
 
 $prefix="";
 $db = new SQLite3('../tracks.sqlite');
-//$TrackerID = $db->query('SELECT id FROM locations WHERE date >= DateTime(\'now\',\'-10 minutes\') AND latitude != "" GROUP BY id');
 $TrackerID = $db->query('SELECT id FROM locations WHERE date >= DateTime(\'now\',\'-10 minutes\') AND latitude != "" AND id == "gps-tracker-2" GROUP BY id');
 
-        while ( $ID = $TrackerID->fetchArray() ) {
-            $stmt = $db->querySingle("SELECT date, id, altitude, latitude, longitude, battery FROM locations WHERE latitude != \"\" AND id = \"$ID[0]\" ORDER BY date DESC LIMIT 1", true);
-            echo $prefix; ?>
-        {
-            "tracker-id": "<?php echo $stmt['id']; ?>",
-            "date": "<?php echo $stmt['date']; ?>",
-            "battery": "<?php echo $stmt['battery']; ?>",
-            "location": { 
-                "altitude": "<?php echo $stmt['altitude']; ?>",
-                "latitude": "<?php echo $stmt['latitude']; ?>",
-                "longitude": "<?php echo $stmt['longitude']; ?>"
-            }
-        }<?php $prefix=","; } ?>
+while ( $ID = $TrackerID->fetchArray() ) {
+$stmt = $db->querySingle("SELECT date, id, altitude, latitude, longitude, battery FROM locations WHERE latitude != \"\" AND id = \"$ID[0]\" ORDER BY date DESC LIMIT 1", true); ?>
+{
+    "tracker-id": "<?php echo $stmt['id']; ?>",
+    "date": "<?php echo $stmt['date']; ?>",
+    "battery": "<?php echo $stmt['battery']; ?>",
+    "location": { 
+        "altitude": "<?php echo $stmt['altitude']; ?>",
+        "latitude": "<?php echo $stmt['latitude']; ?>",
+        "longitude": "<?php echo $stmt['longitude']; ?>"
+    }
+}<?php } ?>
